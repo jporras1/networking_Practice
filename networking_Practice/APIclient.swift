cda4ed4221342e23934d35640ee6e10ab1cfdaca
jporras1 <javi.jr2150@gmail.com>
2019-11-06 07:32:20 -0600

    Initial commit
    Finished the implementation of the networking layer and got a good amount of practice doing so.

---
+++ networking_Practice/APIclient.swift
@@ -0,0 +1,43 @@
+//
+//  APIclient.swift
+//  networking_Practice
+//
+//  Created by Javier Porras jr on 11/5/19.
+//  Copyright © 2019 Javier Porras jr. All rights reserved.
+//
+
+import UIKit
+
+class APIclient {
+    fileprivate let baseURL = "http://www.javierporrasjr.com/pokemon/pokemon.json"
+    
+    func fetchPokemon(completion: @escaping (Pokemon)->()){
+        guard let url = URL(string: baseURL) else {return}
+        let request = URLRequest(url: url)
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            do {
+                if let data = data, let json = try JSONSerialization.jsonObject(with: data, options: []) as? [Any]{
+                    if let dictionary = json.first as? [String : AnyObject]{
+                        let pokemon = Pokemon(dictionary: dictionary)
+                        guard let imageURL = pokemon.imageURL else {return}
+                        self.fetchImage(urlString: imageURL, completion: { (image) in
+                            pokemon.image = image
+                            completion(pokemon)
+                        })
+                    }
+                }
+            }catch let JSONerror{
+                print(JSONerror)
+            }
+        }.resume()
+    }//End Of Fetch Pokemon method
+    private func fetchImage(urlString: String, completion: @escaping (UIImage)->()){
+        guard let url = URL(string: urlString) else {return}
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {return}
+            guard let image = UIImage(data: data) else {return}
+            completion(image)
+        }.resume()
+    }//End of fetch image method
+}//End of APIclient
+
--- networking_Practice/ViewController.swift
+++ networking_Practice/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let client = APIclient()
+    var pokemon: Pokemon? {
+        didSet{
+            label.text = pokemon?.name
+            imageView.image = pokemon?.image
+        }
+    }
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.textColor = .white
+        label.font = .boldSystemFont(ofSize: 24)
+//        label.backgroundColor = .blue
+//        label.text = "Bulbazar"
+        return label
+    }()
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+//        imageView.backgroundColor = .red
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .gray
+        view.addSubview(label)
+        view.addSubview(imageView)
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 90).isActive = true //(equalTo: view.centerYAnchor).isActive = true
+        label.widthAnchor.constraint(equalToConstant: 350).isActive = true
+        label.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: label.topAnchor, constant: -30).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 250).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 250).isActive = true
+        client.fetchPokemon { (pokemon) in
+            DispatchQueue.main.async {
+                self.pokemon = pokemon
+            }
+        }
     }
-
-
 }
 
